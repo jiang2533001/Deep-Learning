54b3f8e7dedd4878a458a8867dded7a617ce09db
Zhi Jiang <jiang2533001@gmail.com>
2018-01-03 20:01:02 -0800

    update preprocess

--- ANN/data_preprocess.py
+++ ANN/data_preprocess.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 
 def preprocess(filename):
     data_set = pd.read_csv(filename)
-    x = datat_set.iloc[:, 1:13].values
-    print encode_categorical_date(x[:, 1])
+    x = data_set.iloc[:, 3:13].values
+    y = data_set.iloc[:, 14].values
 
-
-
-
+    x[:, 1] = encode_categorical_data(x[:, 1])
+    x[:, 2] = encode_categorical_date(x{:, 2})
 
 def encode_categorical_data(list):
     c_dict = {}
@@ -21,6 +20,6 @@ def encode_categorical_data(list):
             c_dict[x] = num
             num += 1 
         new_list.append(c_dict[x])
-     
+    
     return new_list
         
