152940b555706a0fd18e77ef9dc838346a324ed7
Zhi Jiang <jiang2533001@gmail.com>
2018-01-03 19:54:55 -0800

    create a program for ANN

---
+++ ANN/data_preprocess.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def preprocess(filename):
+    data_set = pd.read_csv(filename)
+    x = datat_set.iloc[:, 1:13].values
+    print encode_categorical_date(x[:, 1])
+
+
+
+
+
+def encode_categorical_data(list):
+    c_dict = {}
+    num = 1
+    new_list = []
+
+    for x in list:
+        if c_dict.has_key(x) == False:
+            c_dict[x] = num
+            num += 1 
+        new_list.append(c_dict[x])
+     
+    return new_list
+        
---
+++ ANN/main.py
@@ -0,0 +1,7 @@
+import data_preprocess as data
+
+def main():
+    data.preprocess('data_set.csv')
+
+if __name__ == '__main__':
+    main()
