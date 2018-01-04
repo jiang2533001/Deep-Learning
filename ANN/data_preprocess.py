0a5adb250815cae87ce592b37c6403f4b3f2ee7d
jiang2533001 <jiang2533001@gmail.com>
2018-01-03 21:27:23 -0800

    delete hidden files

--- ANN/data_preprocess.py
+++ ANN/data_preprocess.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 
 def preprocess(filename):
     data_set = pd.read_csv(filename)
-    x = data_set.iloc[:, 3:13].values
-    y = data_set.iloc[:, 14].values
+    x = datat_set.iloc[:, 1:13].values
+    print encode_categorical_date(x[:, 1])
 
-    x[:, 1] = encode_categorical_data(x[:, 1])
-    x[:, 2] = encode_categorical_date(x{:, 2})
 
 def encode_categorical_data(list):
     c_dict = {}
@@ -20,6 +18,6 @@ def encode_categorical_data(list):
             c_dict[x] = num
             num += 1 
         new_list.append(c_dict[x])
-    
-    return new_list
+     
+     return new_list
         
