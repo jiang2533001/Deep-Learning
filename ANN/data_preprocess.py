9d6971134efc7a723c8405f42ad74798c1067878
jiang2533001 <jiang2533001@gmail.com>
2018-01-04 12:09:38 -0800

    add new stuff

--- ANN/data_preprocess.py
+++ ANN/data_preprocess.py
@@ -1,20 +1,26 @@
 import pandas as pd
 import numpy as npi
 import matplotlib.pyplot as plt
-import sklearn.preprocessing import StandardScaler
+from  sklearn.preprocessing import StandardScaler
 
-def read_data(filename, range_1, range_2):
+def read_data(filename, unneeded, encode):
     data_set = pd.read_csv(filename)
-    needed_set = data_set[:, range_1:range_2].values
+    for column in unneeded:
+        del data_set[column]
     
-    return needed_set
+    for column in encode:
+        data_set[column]= encode_categorical_data(data_set[column])
+
+    final_set = data_set.values
+    return final_set
     
-def encode_categorical_data(list):
+def encode_categorical_data(data):
+    column = data.values
     c_dict = {}
     num = 1
     new_list = []
 
-    for x in list:
+    for x in column:
         if c_dict.has_key(x) == False:
             c_dict[x] = num
             num += 1 
@@ -22,12 +28,14 @@ def encode_categorical_data(list):
     
     return new_list
     
-
 def normalize_data(matrix):
     sc = StandardScaler()
     matrix = sc.fit_transform(matrix)
 
     return matrix
 
-def train_test_split(matrix):
+def train_test_split(matrix, ratio):
+    total = len(matrix)
+    train_data = int(ratio * total)
 
+    return matrix[0:train_data, :], matrix[train_data:, :]
